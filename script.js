e15165c67e214cd3c8e88617d7a66e37bc040393
LBeizaIndo <lfbeiza@miuandes.cl>
2024-03-18 13:36:16 -0300

    lab 2: changes

---
+++ script.js
@@ -0,0 +1,4 @@
+function changeSection() {
+    var inputValue = document.getElementById("textInput").value;
+    document.getElementById("change-section").innerHTML = "<p>" + inputValue + "</p>";
+}
